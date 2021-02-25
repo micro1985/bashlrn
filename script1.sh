65cd58601f971976dda4de88a3a77eba44f153f2
Sasha Gatsiha <micro1985@mail.ru>
2021-02-25 19:19:13 +0300

    first func

--- script1.sh
+++ script1.sh
@@ -3,3 +3,28 @@
 osname=`hostname`
 
 echo ${osname}
+
+f_server_install()
+{
+	sudo yum install nfs-utils nfs-utils-lib
+	sudo systemctl enable rpcbind
+	sudo systemctl enable nfs-server
+	systemctl start rpcbind
+	systemctl start nfs-server
+
+	sudo mkdir /home/user/nfsbckp
+	sudo chmod -R 777 /home/user/nfsbckp
+
+	echo "/home/user/nfsbckp 192.168.40.0/24 (rw,sync,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
+
+	sudo exports -a
+	sudo systemctl restart nfs-server
+}
+
+
+if [ "${osname}" == "server" ]; then
+	echo ${osname}
+elif [ "${osname}" == "client" ]; then
+	echo ${osname}
+fi
+
