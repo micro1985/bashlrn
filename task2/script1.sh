8765326e49cf75ceaaadaff4e14a4a40505bf9a3
Sasha Gatsiha <micro1985@mail.ru>
2021-02-25 21:27:49 +0300

    changes

--- task2/script1.sh
+++ task2/script1.sh
@@ -2,4 +2,4 @@
 
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
-sudo yum install zabbix-server-mysql
+sudo yum install zabbix-server-mysql zabbix-web-mysql
