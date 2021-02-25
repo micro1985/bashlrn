8a59d616d577f038a6eb556eb1a433093abd247e
Sasha Gatsiha <micro1985@mail.ru>
2021-02-25 20:48:30 +0300

    working second script

--- script2.sh
+++ script2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+date=`date '+%Y%m%d%H%M%S'`
+filename=backup_$date
+
+sudo tar -czvf $filename.tar.gz /root /home
+sudo cp $filename.tar.gz /home/user/backup
+sudo rm $filename.tar.gz
