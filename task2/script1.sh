806550737644ed7b448864b1bea6bfe48a60ff38
Sasha Gatsiha <micro1985@mail.ru>
2021-02-26 13:04:04 +0300

    mysql

--- task2/script1.sh
+++ task2/script1.sh
@@ -2,7 +2,7 @@
 
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
-sudo yum install zabbix-server-mysql zabbix-web-mysql mariadb-server
+sudo yum install zabbix-server-mysql zabbix-web-mysql mariadb-server -y
 
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
@@ -18,6 +18,6 @@ EOF
 
 sudo mysql -u root -e "CREATE DATABASE zabbix character set utf8 collate utf8_bin;"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON zabbix.* TO zabbix@localhost identified by '123456';"
-#sudo mysql -u root -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
-sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | sudo mysql -u zabbix -p=123456
+sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | sudo mysql -u zabbix -p=123456 zabbix
