96f597346960162f067099af92f089db69a9ea20
Sasha Gatsiha <micro1985@mail.ru>
2021-02-27 13:54:21 +0300

    client conf added

--- task2/script2.sh
+++ task2/script2.sh
@@ -13,5 +13,13 @@ sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
 
 sudo yum install zabbix-agent -y
+
+sudo sed -i "/\# Server=/d" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/Server=/c Server=192.168.40.161" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/\# ServerActive=/d" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/ServerActive=/c ServerActive=192.168.40.161" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/\# Hostname=/d" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/Hostname=/c Hostname=Client" /etc/zabbix/zabbix_agentd.conf
+
 sudo systemctl start zabbix-agent
 sudo systemctl enable zabbix-agent
