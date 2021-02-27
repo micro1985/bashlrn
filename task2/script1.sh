422c99acd01976011ce31b1a22ba96f373a9cac2
Sasha Gatsiha <micro1985@mail.ru>
2021-02-27 11:09:38 +0300

    some coments

--- task2/script1.sh
+++ task2/script1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+###Checking for installed Zabbix
+
 cheeck=`sudo yum list installed | grep zabbix`
 
 if [ "${cheeck}" != "" ]; then
@@ -7,9 +9,13 @@ if [ "${cheeck}" != "" ]; then
 	exit
 fi
 
+###Installing packages
+
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
 sudo yum install zabbix-server-mysql zabbix-web-mysql mariadb-server -y
+
+###Configurinf DB
 
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
@@ -27,7 +33,11 @@ sudo mysql -u root -e "create database zabbix character set utf8 collate utf8_bi
 sudo mysql -u root -e "create user 'zabbix'@'localhost' identified by '123456';"
 sudo mysql -u root -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
 
+###Importing data
+
 sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | sudo mysql -u zabbix --password=123456 zabbix
+
+###Configuring zabbix config file
 
 sudo sed -i "/\# DBHost=/c DBHost=localhost" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/DBHost=/c DBHost=localhost" /etc/zabbix/zabbix_server.conf   #for universality
@@ -37,6 +47,8 @@ sudo sed -i "/\# DBUser=/d" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/DBUser=/c DBUser=zabbix" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/\# DBPassword=/c DBPassword=123456" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/DBPassword=/c DBPassword=123456" /etc/zabbix/zabbix_server.conf
+
+###Starting zabbix
 
 sudo systemctl start zabbix-server httpd
 sudo systemctl enable zabbix-server httpd
