1ce46a6005e02172789a2533b4ff29cb39472f6a
Sasha Gatsiha <micro1985@mail.ru>
2021-02-27 09:31:05 +0300

    check

--- task2/script1.sh
+++ task2/script1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cheeck='systemctl status zabbix-server | grep "Active: active"'
+
+if [ "${cheeck}" != "" ]; then
+	echo "Zabbix is already installed"
+	exit
+fi
+
 sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
 sudo yum install zabbix-server-mysql zabbix-web-mysql mariadb-server -y
