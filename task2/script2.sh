548594517f0c7ec1111b05bba082ca042b5db853
Sasha Gatsiha <micro1985@mail.ru>
2021-02-27 15:09:57 +0300

    sdfsggdfg

--- task2/script2.sh
+++ task2/script2.sh
@@ -14,9 +14,9 @@ sudo yum-config-manager --enable rhel-7-server-optional-rpms
 
 sudo yum install zabbix-agent -y
 
-#sudo sed -i "/\# Server=/d" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/Server=127.0.0.1/d" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "/\# Server=/c Server=192.168.40.161" /etc/zabbix/zabbix_agentd.conf
-#sudo sed -i "/\# ServerActive=/d" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "/ServerActive=127.0.0.1/d" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "/\# ServerActive=/c ServerActive=192.168.40.161" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "/\# Hostname=/d" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "/Hostname=/c Hostname=Client" /etc/zabbix/zabbix_agentd.conf
