d1ab67dc2fd4e7e7542a4943c0cec502275376de
micro1985 <74548507+micro1985@users.noreply.github.com>
2021-03-09 08:43:17 +0300

    Added echos

--- task2/script1.sh
+++ task2/script1.sh
@@ -15,10 +15,14 @@ sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.
 sudo yum-config-manager --enable rhel-7-server-optional-rpms
 sudo yum install zabbix-server-mysql zabbix-web-mysql mariadb-server httpd httpd-devel php php-devel php-bcmath php-pear php-gd php-mbstring php-mysql php-xml  -y
 
+echo "Packages installed"
+
 ###Configurinf DB
 
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+
+echo "DB started and autorunned"
 
 cat << EOF | sudo mysql_secure_installation
 
@@ -33,14 +37,20 @@ sudo mysql -u root -e "create database zabbix character set utf8 collate utf8_bi
 sudo mysql -u root -e "create user 'zabbix'@'localhost' identified by '123456';"
 sudo mysql -u root -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
 
+echo "DB configured"
+
 ###Importing data
 
 sudo zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | sudo mysql -u zabbix --password=123456 zabbix
+
+echo "Zabbix server config imported to DB"
 
 ###Disabling SELinux
 
 sudo sed -i "/SELINUX=enforcing/c SELINUX=disabled" /etc/selinux/config
 sudo setenforce 0
+
+echo "SELinux disabled"
 
 ###Configuring zabbix config file
 
@@ -53,11 +63,17 @@ sudo sed -i "/DBUser=/c DBUser=zabbix" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/\# DBPassword=/c DBPassword=123456" /etc/zabbix/zabbix_server.conf
 sudo sed -i "/DBPassword=/c DBPassword=123456" /etc/zabbix/zabbix_server.conf
 
+echo "Zabbix configured"
+
 ###Configuring Zabbix conf for Apache
 
 sudo sed -i "/\# php_value date.timezone/c \ \ \ \ \ \ \ \ php_value date.timezone Europe/Minsk" /etc/httpd/conf.d/zabbix.conf
+
+echo "Apache configured for Zabbix"
 
 ###Starting zabbix
 
 sudo systemctl start zabbix-server httpd
 sudo systemctl enable zabbix-server httpd
+
+echo "Zabbix started and autorunned"
