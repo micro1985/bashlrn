2e7ce67c1d9003d82233378e28aedfbad3738fa3
Gatsiha Alexander <micro1985@SKU2.Agat.by>
2021-03-15 10:28:15 +0300

    added 3rd

---
+++ task2/pass_3.py
@@ -0,0 +1,25 @@
+from smart_env import ENV
+from random import randint
+
+cort = ("a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
+        "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N",
+        "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U",
+        "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "1", "2", "3", "4",
+        "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*",
+        "(", ")")
+ps = ""
+
+for i in range(12):
+    k = randint(0, len(cort))
+    ps += cort[k]
+
+print(ps)
+
+ENV.enable_automatic_type_cast()
+
+ENV.PS_VAR = ps
+
+print(ENV.PS_VAR)
+#psfile = open("psswd", 'w')
+#psfile.write(ps)
+#psfile.close()
